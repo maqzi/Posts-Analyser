b198189187377c7d58131cd7e5e950981353a3f4
maqzi <munaf.qazi@gmail.com>
2017-04-12 22:58:44 -0400

    added xml parser and iot data as another test set

---
+++ XML2CSV.py
@@ -0,0 +1,16 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+# xml_data = 'iot.stackexchange/Posts.xml'
+xml_data = 'ai.stackexchange/Posts.xml'
+
+def xml2df(xml_data):
+    tree = ET.parse(xml_data)
+    root = tree.getroot()
+    allRecords = []
+    for child in root:
+        allRecords.append(child.attrib)
+    return pd.DataFrame(allRecords)
+
+data = xml2df(xml_data)
+data.to_csv('ai_posts.csv')
