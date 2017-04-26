c947042bc06d8d6a7f301bf13e63ee26773917b3
Daniel Farnand <dfarnand@nyu.edu>
2017-04-25 21:36:03 -0400

    Expanded the AddingReadibility.py script to call the text cleaning function before calculating readibility indices and saving the text to the csv. Also saves columns of the total length of code, latex, and punctuation in each.

--- AddingReadibility.py
+++ AddingReadibility.py
@@ -1,26 +1,40 @@
 # Date: 04/21/2017
+# Updated DFarnand 4/27/2017
+from TextCleaning import TextCleaning
 from textstat.textstat import textstat
 import pandas as pd
 def AddReadabilityMeasures(filename):
-    df = pd.read_csv('/Users/amanmahato/Desktop/Local Folder/Posts-Analyser/'+filename+'.csv')
+    df = pd.read_csv(filename+'.csv') #Should be in the same working directory?
     Flesch_Reading_Ease_Value=[]
     Coleman_Liau_Index_Value=[]
     Dale_Chall_Readability_Score=[]
+    Code_Count=[]
+    Latex_Count=[]
+    Punc_Count=[]
     for text in df['Body']:
         if(isinstance(text, str)):
+            cleaned = TextCleaning(text)
+            text = cleaned['text']
             flesch_reading_ease = textstat.flesch_reading_ease(text)
             coleman_liau_index = textstat.coleman_liau_index(text)
             dale_chall_readability_score = textstat.dale_chall_readability_score(text)
-        else:
+        else: #Consider using some sort of NA value? We probably just want to remove these in the analysis anyway
+            cleaned = TextCleaning('') # Hacky way have values below
             flesch_reading_ease = 0
             coleman_liau_index =0
             dale_chall_readability_score =0
         Flesch_Reading_Ease_Value.append(flesch_reading_ease)
         Coleman_Liau_Index_Value.append(coleman_liau_index)
         Dale_Chall_Readability_Score.append(dale_chall_readability_score)
+        Code_Count.append(cleaned['codeLen'])
+        Latex_Count.append(cleaned['latLen'])
+        Punc_Count.append(cleaned['punLen'])
     df['Flesch_Reading_Ease_Value']=Flesch_Reading_Ease_Value
     df['Coleman_Liau_Index_Value']=Coleman_Liau_Index_Value
     df['Dale_Chall_Readability_Score']=Dale_Chall_Readability_Score
+    df['Code_Count']=Code_Count
+    df['Latex_Count']=Latex_Count
+    df['Punc_Count']=Punc_Count
     return df
 dataFrameAi = AddReadabilityMeasures('ai_posts')
 dataFrameIot=AddReadabilityMeasures('iot_posts')
