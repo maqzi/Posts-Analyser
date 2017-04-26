bb22d1b6fae27f1519560b5e73fb31684134bb7a
Daniel Farnand <dfarnand@nyu.edu>
2017-04-25 21:49:54 -0400

    Removed unnecessary testing code from TextCleaning

--- TextCleaning.py
+++ TextCleaning.py
@@ -39,28 +39,3 @@ def TextCleaning(text):
 
     return {'text':text, 'codeLen':codeLen ,'latLen':latLen, 'punLen':punLen}
 
-
-
-test = ("\n"
-	"<p>I'm doing analysis using Proton-transfer-reaction mass spectrometry techniques. I've collected data over time (5 measurement steps and repeated samplings for each measurement time). \n"
-	"For each measure I observe normalized counts-per-second (ncps) associated with different molecula. I also have a qualitative property associated with my measure. </p>\n\n"
-	"<p>So my data set is something like:</p>\n\n"
-	"<pre><code>Time (express in days, starting from day 0), Sample (numeric), Type (char), Molecula1 (ncps),...., Molecula40 (ncps)\n"
-	"</code></pre>\n\n"
-	"<p>I need to conduct a statistical analysis with my data set.\n"
-	"I'd like to perform Anova to determine witch molecula are statistically relevant. \n"
-	"Can I use 1-way Anova? (I could perform one way anova referring to \"Time\", and also to \"Type\") or could I use 2-way anova?\n"
-	"After this step I'll get the moleculas that aren't redundant in my data set.</p>\n\n"
-    "<p>Suppose we have a polynomial $xy+f(x,y)$, where $f(x,y)$ is a polynomial in $\\mathbb C[x,y]$ whose the lowest degree term has degree at least 3. My question is, are we always able to decompose $xy+f(x,y)$ into the product of two convergent power series $x+g(x,y)$ and $y+h(x,y)$ in a neighborhood of $(0,0)$, where terms in $g$ and $h$ have order higher than 1?</p> \n"
-"<p>I have no idea about the convergence of power series with two or more variables. Any solution or reference will be appreciated! Please also note that it's not enough to simply decompose $xy+f(x,y)$ formally into two $x+g(x,y)$ and $y+h(x,y)$. We also need the convergence of $g$ and $h$! </p> \n"
-	"<p>Am I right?</p>\n\n"
-	"<p>Then I want to use PCA analysis to reduce my independent variables</p>\n\n"
-	"<pre><code>Molecula1,...., Molecula40\n"
-	"</code></pre>\n\n"
-	"<p>Is PCA the right approach or MFA (using FactoMineR ) is better?</p>\n\n"
-	"<p>Thanks </p>\n"
-	"    </div>\n")
-
-print(TextCleaning("Here's some text with nothing specific"))
-outcome = TextCleaning(test)
-print(outcome)
