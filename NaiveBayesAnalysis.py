8c0e8c4b5bd50b2592c9dfb941432684a42bcec1
maqzi <munaf.qazi@gmail.com>
2017-04-28 14:23:16 -0400

    bagged a NaiveBayes with the Random Forest (pickle not uploaded)

---
+++ NaiveBayesAnalysis.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+def remove_punct(desc):
+    import string
+    import re
+    t = re.sub('[' + string.punctuation + ']', '', str(desc).lower())
+    return t
+
+def NaiveBayesGuess(df,filename):
+    from textblob.classifiers import NaiveBayesClassifier
+    import pickle
+    texts = df['Clean_Text']
+    scores = df['ScoreLabel']
+    text_tuples = []
+
+    # Create Text Tuples
+    for i,text in enumerate(texts):
+        text = remove_punct(text)
+        text_tuples.append(tuple((text,scores[i])))
+
+    # Train a Naive Bayes Classifier (NLTK implementation) on normalized texts
+    # nbcl = NaiveBayesClassifier(text_tuples)
+
+    # 'Pickle' the classifier for future use
+    # f = open('nbcl_'+filename+'.pickle', 'wb')
+    # pickle.dump(nbcl, f)
+    # f.close()
+
+    # Load Pickled Classifier
+    loaded_nbcl = pickle.load(open('nbcl_'+filename+'.pickle', 'rb'))
+
+    # Check the classifier's predictions for the test set
+    # nbcl_acc = loaded_nbcl.accuracy(text_tuples)
+    # print('NB description classifier: {}'.format(nbcl_acc))
+
+    # Classify all descriptions and write class values to file
+    pd.DataFrame(loaded_nbcl.classify(remove_punct(text)) for text in texts).to_csv('nb_'+filename+'_guess.csv',index='False',header='False')
+
+
+iot = 'iot_posts_with_readibility_measures'
+# ai = 'ai_posts_with_readibility_measures'
+df = pd.read_csv(iot+'.csv', sep=",")
+df.fillna(np.NaN, inplace=True)
+NaiveBayesGuess(df,iot)
