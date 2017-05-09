d240762fd4dd75698038f6c36f8ceb61083500c3
Daniel Farnand <dfarnand@nyu.edu>
2017-05-08 20:49:54 -0400

    Lots of additions to the LogReg, including Baseline Calculations!

--- LogReg.py
+++ LogReg.py
@@ -62,7 +62,7 @@ def LogMod(filename, features, outcome, cutoff, vect=False):
 
     outc = dataset.ix[:,outcome]
 
-    istest = np.repeat(np.repeat([False,True],[9,1]), 1+len(outc)/10)[:len(outc)]
+    istest = np.repeat(np.repeat([False,True],[8,2]), 1+len(outc)/10)[:len(outc)]
     np.random.shuffle(istest)
 
     logReg = LogisticRegression()
@@ -82,11 +82,12 @@ def LogModVec(filename, outcome, textName, count=True):
     outc = df.ix[:,outcome]
 
     row = df.shape[0]
-    istest = np.repeat(np.repeat([False,True],[9,1]), 1+row/10)[:row]
+    istest = np.repeat(np.repeat([False,True],[8,2]), 1+row/10)[:row]
     np.random.shuffle(istest)
     #pdb.set_trace()
 
     if count: vect = CountVectorizer()
+    #else: vect = TfidfVectorizer(ngram_range=(1,3))
     else: vect = TfidfVectorizer()
     fitted = vect.fit_transform(df.ix[~istest,textName])
     test = vect.transform(df.ix[istest,textName])
@@ -98,40 +99,126 @@ def LogModVec(filename, outcome, textName, count=True):
     
     return(ReportCard(outc[istest],pred,probPred))
 
+def LogRegComb(filename, feature, outcome, textName, count=True):
+    """Just gets predictions from tfidf vectorization, then uses that as a
+    predictor for the 
+    """
+    # Initial Dataframe for Vectors
+    df = pd.read_csv(filename+'.csv', sep=",")
+    df = df.ix[:,[textName]+[outcome]]
+    df = df.dropna()
+
+    # Outcome for all data
+    outc = df.ix[:,outcome]
+
+    # Training and Test Indices
+    row = df.shape[0]
+    istest = np.repeat(np.repeat([False,True],[8,2]), 1+row/10)[:row]
+    np.random.shuffle(istest)
+
+    # Running Vectorization on Training set
+    vect = TfidfVectorizer()
+    fitted = vect.fit_transform(df.ix[~istest,textName])
+
+    # Fitting vectorization to log regression and getting prediction
+    logReg = LogisticRegression()
+    logReg.fit(fitted, outc[~istest])
+
+
+    # Combining predictions with other features and fitting final reg
+    dataset = PrepData(filename, features,outcome)
+    feats = dataset.ix[~istest,features] # Feats is JUST TRAINING
+    feats['VecPred'] = logReg.predict_proba(fitted)[:,1]
+    logReg2 = LogisticRegression()
+    logReg2.fit(feats, outc[~istest])
+
+    # Plugging in the test data
+    test = vect.transform(df.ix[istest,textName])
+    testFeats = dataset.ix[istest,features]
+    testFeats['VecPred'] = logReg.predict_proba(test)[:,1]
+    preds = logReg2.predict(testFeats)
+    probPreds = logReg2.predict_proba(testFeats)
+    return(ReportCard(outc[istest],preds,probPreds))
+
+def baseline(filename, binOutcome):
+        ## Prints the result statistics for a baseline model
+        out = pd.read_csv(filename+'.csv', sep=",")[binOutcome]
+        staticAssign = np.repeat(0,len(out)) # Assigning all to the most common value
+        if (out.value_counts()[0] < out.value_counts()[1]): staticAssign += 1
+
+        blRep1 = ReportCard(out, staticAssign, np.vstack((1-staticAssign,staticAssign)).T)
+
+        randomAssign = np.random.choice([0,1],len(out)) # Looking at random assignment
+        blRep2 = ReportCard(out, randomAssign, np.vstack((1-randomAssign,randomAssign)).T)
+
+        print(blRep1['accuracy'], blRep1['auc'], blRep1['log_loss'])
+        print(blRep1['report'])
+
+        ## Decided I don't really need the random assignment, its all just 50%
+        #rint(blRep2['accuracy'], blRep2['auc'], blRep2['log_loss'])
+        #rint(blRep2['report'])
+
+    
 if __name__ == "__main__":
     features = ['Flesch_Reading_Ease_Value', 'Coleman_Liau_Index_Value',
                 'Dale_Chall_Readability_Score',
                 'Polarity', 'Subjectivity',
                 'Code_Count', 'Latex_Count','Punc_Rate'] # Have to put outcome Y in based on above function
-    iot = 'iot_posts_with_readibility_measures'
-    ai = 'ai_posts_with_readibility_measures'
-    stats = 'stats_posts_with_readibility_measures'
+    iot = 'iot_posts_with_readibility_measures_score_adj'
+    ai = 'ai_posts_with_readibility_measures_score_adj'
+    stats = 'stats_posts_with_readibility_measures_score_adj'
 
-    # print(xValLog(iot,features, k=10, cutoff=2)) # Just calculated with specific features
-    # print(xValLog(ai,features, k=10, cutoff=2))
-    # print(xValLog(stats,features, k=10, cutoff=2))
+    #print(xValLog(iot,features, k=10, cutoff=2)) # Just calculated with specific features
+    #print(xValLog(ai,features, k=10, cutoff=2))
+    #print(xValLog(stats,features, k=10, cutoff=2))
 
-
-    ##Testing cutoffs for ai
-    #for i in range(10):
-    #    print(xValLog(ai,features, k=10, cutoff=i))
-    # Doesn't make much of a difference. I think this can be based more on our
-    # own subjective definition of what a "high score" is
-
+    ## Testing cutoffs for ai
+    ## for i in range(10):
+    ##    print(xValLog(ai,features, k=10, cutoff=i))
+    ## Doesn't make much of a difference. I think this can be based more on our
+    ## own subjective definition of what a "high score" is
 
     iotRep = LogMod(iot,features,'ScoreLabel', cutoff=2)
     aiRep = LogMod(ai,features,'ScoreLabel', cutoff=2)
     statsRep = LogMod(stats,features,'ScoreLabel', cutoff=2)
-
+    print("Using Features")
     print(iotRep['accuracy'],aiRep['accuracy'],statsRep['accuracy'])
     print(iotRep['auc'],aiRep['auc'],statsRep['auc'])
 
+    ## Vectorized Count
+    iotRepVec = LogModVec(iot,'ScoreLabel','Clean_Text',count=True)
+    aiRepVec = LogModVec(ai,'ScoreLabel','Clean_Text',count=True)
+    statsRepVec = LogModVec(stats,'ScoreLabel','Clean_Text',count=True)
 
-    ## Vectorized
-    iotRepVec = LogModVec(iot,'ScoreLabel','Clean_Text',count=False)
-    aiRepVec = LogModVec(ai,'ScoreLabel','Clean_Text',count=False)
-    statsRepVec = LogModVec(stats,'ScoreLabel','Clean_Text',count=False)
-
+    print("Count Vectorizer")
     print(iotRepVec['accuracy'],aiRepVec['accuracy'],statsRepVec['accuracy'])
     print(iotRepVec['auc'],aiRepVec['auc'],statsRepVec['auc'])
 
+    ## Vectorized TF-IDF
+    iotRepVecT = LogModVec(iot,'ScoreLabel','Clean_Text',count=False)
+    aiRepVecT = LogModVec(ai,'ScoreLabel','Clean_Text',count=False)
+    statsRepVecT = LogModVec(stats,'ScoreLabel','Clean_Text',count=False)
+
+    print("TF-IDF Vectorizer")
+    print(iotRepVecT['accuracy'],aiRepVecT['accuracy'],statsRepVecT['accuracy'])
+    print(iotRepVecT['auc'],aiRepVecT['auc'],statsRepVecT['auc'])
+
+    iotRepComb = LogRegComb(iot,features,'ScoreLabel','Clean_Text',count=False)
+    aiRepComb = LogRegComb(ai,features,'ScoreLabel','Clean_Text',count=False)
+    statsRepComb = LogRegComb(stats,features,'ScoreLabel','Clean_Text',count=False)
+
+    print("Two-stage Log Regression")
+    print(iotRepComb['accuracy'],aiRepComb['accuracy'],statsRepComb['accuracy'])
+    print(iotRepComb['auc'],aiRepComb['auc'],statsRepComb['auc'])
+
+
+# ## Saved Here Just in Case
+# Using Features
+# 0.796116504854 0.603773584906 0.67629671516
+# 0.808946877912 0.469555035129 0.578797290609
+# Count Vectorizer)
+# 0.912621359223 0.584905660377 0.683287924742
+# 0.899100899101 0.542711864407 0.637123451748
+# TF-IDF Vectorizer
+# 0.893203883495 0.62893081761 0.702719374904
+# 0.828205128205 0.640425531915 0.67918525628
